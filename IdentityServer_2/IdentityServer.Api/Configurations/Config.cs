a723533759c0bdc855f431d177bfb1f21581bb28
Serhat <srhtayata@gmail.com>
2023-04-20 15:07:52 +0300

    IdentityServer_2 init

---
+++ IdentityServer_2/IdentityServer.Api/Configurations/Config.cs
@@ -0,0 +1,20 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace IdentityServer.Api.Configurations
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+              new ApiScope[]
+              {
+                  
+              };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+               
+            };
+    }
+}
